9580b395ceeb5f13a79d020d2f9bd739c1ead20e
shetengteng <work_stt@163.com>
2025-07-20 18:57:00 +0800

    updated

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import './styles/theme.css';
+import './styles/global.css';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
 // 导入Remix Icons
